dde150984991a34a3b7565a83dbb5b9269fe87c8
marcin witkowski <marcin.witkowski@e-qsg.com>
2022-02-20 14:08:45 +0100

    New class

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,12 +1,7 @@
 package introduction;
 
-import org.apache.log4j.Logger;
-
 public class HelloWorld {
-    static Logger log = Logger.getLogger("name");
-
     public static void main(String [ ] args) {
         System.out.println("Hello World");
-        log.info("message");
     }
 }
