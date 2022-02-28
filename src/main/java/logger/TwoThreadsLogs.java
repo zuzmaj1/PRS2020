5cf487abe4520adac8bc3fa1f08841d680bd420c
s461009 <zuzmaj1@st.amu.edu.pl>
2022-02-28 18:39:41 +0100

    zajęcia1

--- src/main/java/logger/TwoThreadsLogs.java
+++ src/main/java/logger/TwoThreadsLogs.java
@@ -6,6 +6,7 @@ public class TwoThreadsLogs {
 
     public static Logger log = Logger.getLogger(TwoThreadsLogs.class);
     public static Integer i = 0;
+    public static Long jakaś =0L;
 
     public static void main(String[] args) {
 
@@ -36,8 +37,10 @@ public class TwoThreadsLogs {
         public void run() {
             while (i <= 500) {
                 log.info("Loop " + this.loopNum + ", Read: " + i);
-                i = i + 1;
-                log.info("Loop " + this.loopNum + ", Write: " + i);
+                synchronized (jakaś) {
+                    i = i + 1;
+                    log.info("Loop " + this.loopNum + ", Write: " + i);
+                }
             }
         }
     }
--- src/main/java/thread/SeparateThread.java
+++ src/main/java/thread/SeparateThread.java
@@ -5,8 +5,10 @@ public class SeparateThread {
     public static void main(String[] args) {
 
         Concurrency thread1 = new Concurrency(1); // create an object of class Concurrent which is a thread
-
         thread1.start();
+
+        Concurrency thread2 = new Concurrency(2); // create an object of class Concurrent which is a thread
+        thread2.start();
     }
 
     static class Concurrency extends Thread { // this class is a thread in Java, elements in run method will be run on separate Thread
@@ -21,12 +23,6 @@ public class SeparateThread {
         public void run() {
 
             for (int i = 1; i <= 5; i++) {
-
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    System.out.println(e);
-                }
                 System.out.println("Loop " + this.loopNum + ", Iteration: " + i);
             }
         }
